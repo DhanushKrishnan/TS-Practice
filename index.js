69d9b0ec122490a93f005569f95855ebdfe892c7
Dhanush <dhanushkrish54@gmail.com>
2023-12-18 13:31:18 +0000

    update 7:00 PM

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Dhanush is practicing TS");
+for (let i = 1; i < 4; i++) {
+    console.log(i);
+}
+function addOne(num) {
+    return num += 1;
+}
+var sum = addOne(10);
+console.log(sum);
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
 console.log("Dhanush is practicing TS");
+
+for(let i=1; i<4; i++){
+    console.log(i);
+}
+
+function addOne(num: number){
+    return num+=1;
+}
+
+// let userInput = readline("Enter no: ");
+var sum = addOne(10);
+console.log(sum);
+
+// const double = (x,y) => (x*y);
+export{}
